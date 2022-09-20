05589bdf055df8b520c7c6a4be8355aa21ad6828
Joao Lucas Hilgert <jlhilgert@pm.me>
2022-09-20 14:01:01 +0200

    major update

---
+++ package/f1Package/R/ressources.R
@@ -0,0 +1,9 @@
+f1_reqPackages <- c("httr", "XML","stringr","dplyr","tidyr","ggplot2","data.table","plotly","leaflet","lubridate")
+
+# lap_list <- readRDS("data/F1_lap_history2022.RDS")
+# driverResult <- readRDS("data/driverResult2022.RDS")
+# quali_df <- readRDS("data/quali_results.RDS")
+
+latest_round <- as.numeric(xmlToList(rawToChar(GET("http://ergast.com/api/f1/current/driverStandings")$content))$StandingsTable$StandingsList$.attrs["round"])
+
+#lapply(f1_reqPackages, require, character.only = TRUE)
